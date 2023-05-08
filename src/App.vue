8c6366ec6cd45b2187de45e3f6e7e791dfa0d230
brunaanjos <brunaanjosdaveiga@gmail.com>
2023-05-08 19:43:00 -0300

    12345

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,143 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import { ref } from 'vue'
+
+const listaCompras = ref([
+  
+{
+        id: 1,
+        nome: 'Camiseta',
+        preco: 49.90,
+        quantidade: 0,
+    },
+    {
+        id: 2,
+        nome: 'Calça',
+        preco: 99.90,
+        quantidade: 0,
+    },
+    {
+        id: 3,
+        nome: 'Meia',
+        preco: 9.90,
+        quantidade: 0,
+    },
+    {
+        id: 4,
+        nome: 'Sapato',
+        preco: 199.90,
+        quantidade: 0,
+    },
+    {
+        id: 5,
+        nome: 'Boné',
+        preco: 29.90,
+        quantidade: 0,
+    },
+    {
+        id: 6,
+        nome: 'Óculos',
+        preco: 99.90,
+        quantidade: 0,
+    },
+    {
+        id: 7,
+        nome: 'Relógio',
+        preco: 299.90,
+        quantidade: 0,
+    },
+    {
+        id: 8,
+        nome: 'Bermuda',
+        preco: 79.90,
+        quantidade: 0,
+    },
+    {
+        id: 9,
+        nome: 'Cueca',
+        preco: 19.90,
+        quantidade: 0,
+    },
+    {
+        id: 10,
+        nome: 'Meia',
+        preco: 9.90,
+        quantidade: 0,
+    }
+])
+
+const carrinhos = ref({
+  items: [],
+  total: 0,
+    
+})
+
+function incrementar(index) {
+    listaCompras.value[index].quantidade++
+  }
+  function decrementar(index) {
+    if(listaCompras.value[index].quantidade > 0){
+      listaCompras.value[index].quantidade--
+    }
+  }
+  
+  function adicionar(index) {
+    if(listaCompras.value[index].quantidade > 0){
+      
+      carrinhos.value.items.push(
+        {...listaCompras.value[index], preco: listaCompras.value[index].quantidade * listaCompras.value[index].preco}
+      );
+      
+      carrinhos.value.total = carrinhos.value.total + listaCompras.value[index].preco * listaCompras.value[index].quantidade
+       
+    }
+    
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="produtos">
+    <ul>
+      <li class="lista" v-for="(item, index) in listaCompras" :key="item.id">
+        <p>Item: {{ item.nome }}</p>
+        <p>Preco: {{ item.preco }}</p>
+        <p>ID: {{ item.id }}</p>
+        <p>quantidade: {{ item.quantidade }}</p>
+        <button @click="incrementar(index)">+</button>
+        <button @click="decrementar(index)">-</button>
+        <button @click="adicionar(index)">Adicionar</button>
+      </li>
+    </ul>
+    <hr>
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <div class="carrinhoC">
+  <ul>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <li v-for="item in carrinhos.items">
+        <p>Item: {{ item.nome }}</p>
+        <p>Preço: {{ item.preco.toFixed(2) }}</p>
+        <p>ID: {{ item.id }}</p>
+        <p>Quantidade: {{ item.quantidade }}</p>
+
+       
+    </li>
+
+    <p> Total:{{ carrinhos.total.toFixed(2) }}</p>
+  </ul>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#editor {
+  text-align: left;
+  margin-left: 500px;
+  margin-right: 500px;
+  background-color: rgb(134, 33, 218);
+  border-radius: 50px;
+  border-style: unset;
+  color: rgb(236, 216, 216);
+  padding: 70px
 }
 </style>
