22d2831574824438589d0f50384e03c10c281da4
brunaanjos <brunaanjosdaveiga@gmail.com>
2023-05-12 11:53:31 -0300

    123456

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,77 @@
 <script setup>
 
 import { ref } from 'vue'
-
 const listaCompras = ref([
-  
+
 {
         id: 1,
-        nome: 'Camiseta',
+        nome: 'Livro- Manifesto do partido comunista',
         preco: 49.90,
         quantidade: 0,
+        img:"https://queissocamarada.com/loja/wp-content/uploads/2020/03/camisa-CCCP-vermelha.jpg"
     },
     {
         id: 2,
-        nome: 'Calça',
+        nome: 'Adesivo-Marx',
         preco: 99.90,
         quantidade: 0,
+        img:"https://queissocamarada.com/loja/wp-content/uploads/2018/11/Poster_1.deMaio.png"
     },
     {
         id: 3,
-        nome: 'Meia',
+        nome: 'Body-Fantasma Comunista',
         preco: 9.90,
         quantidade: 0,
+        img:"https://queissocamarada.com/loja/wp-content/uploads/2021/10/Body-verao-Marx-Vermelo.jpg"
     },
     {
         id: 4,
-        nome: 'Sapato',
+        nome: 'Brincos-Foice e Martelo',
         preco: 199.90,
         quantidade: 0,
+        img:"https://queissocamarada.com/loja/wp-content/uploads/2018/08/moletom-branco-ursal-costas.jpg"
     },
     {
         id: 5,
-        nome: 'Boné',
+        nome: 'Camisa-Orgulho de ser Comunista',
         preco: 29.90,
         quantidade: 0,
+        img:"https://ecoms1.com/51315/imgs/big/@v3/1678755854211-foice_martelo-xxilbr.jpg.avif"
     },
     {
         id: 6,
-        nome: 'Óculos',
+        nome: 'Chaveiro-O fantasma ',
         preco: 99.90,
         quantidade: 0,
+        img:"https://ecoms1.com/51315/imgs/big/@v3/1682002614133-256083587_428276985472289_3157863644564957091_n.jpg.avif"
     },
     {
         id: 7,
-        nome: 'Relógio',
+        nome: 'Poster-Lenin',
         preco: 299.90,
         quantidade: 0,
+        img:"https://ecoms1.com/51315/imgs/big/@v3/1679314458079-placa_100.jpg.avif"
     },
     {
         id: 8,
-        nome: 'Bermuda',
+        nome: 'Estatua-Marx',
         preco: 79.90,
         quantidade: 0,
+        img:"https://ecoms1.com/51315/imgs/big/@v3/1679431624226-marx04a.jpg.avif"
     },
     {
         id: 9,
-        nome: 'Cueca',
+        nome: 'Moletom-O fantasma',
         preco: 19.90,
         quantidade: 0,
+        img:"https://ecoms1.com/51315/imgs/big/@v3/1679671783950-angela_01.jpg.avif"
     },
     {
         id: 10,
-        nome: 'Meia',
+        nome: 'Bandeira',
         preco: 9.90,
         quantidade: 0,
+      img:"https://ecoms1.com/51315/imgs/big/@v3/1679433128532-lenin2_02.jpg.avif"  
     }
 ])
 
@@ -111,7 +120,7 @@ function incrementar(index) {
     <hr>
   </div>
 
-  <div class="carrinhoC">
+  <div class="carrinho">
   <ul>
 
     <li v-for="item in carrinhos.items">
@@ -129,15 +138,13 @@ function incrementar(index) {
 </template>
 
 <style scoped>
-
-#editor {
-  text-align: left;
-  margin-left: 500px;
-  margin-right: 500px;
-  background-color: rgb(134, 33, 218);
-  border-radius: 50px;
-  border-style: unset;
-  color: rgb(236, 216, 216);
-  padding: 70px
+ul{
+  width: 300px;
 }
+button{
+  border-color: rgba(13, 13, 14, 0.605);
+}
+
+
+
 </style>
